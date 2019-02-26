a72eaeecc52c6824f1469724874e85f6ec804f2f
Pablo Acosta-Serafini <pmasdev@gmail.com>
2019-02-26 13:00:37 -0500

    Bug fix

--- make-links.sh
+++ make-links.sh
@@ -33,6 +33,7 @@ for fname in ${fnames[*]}; do
     ln -sf "${sbin_dir}/${fname}" "${pkg_dir}/${fname}"
 done
 fname=requirements_to_rst.py
-if [ -d "${pkg_dir}/docs/supprt" ]; then
+if [ -d "${pkg_dir}/docs/support" ]; then
+    echo "Linking ${fname}"
     ln -sf "${sbin_dir}/${fname}" "${pkg_dir}/docs/support/${fname}"
 fi
