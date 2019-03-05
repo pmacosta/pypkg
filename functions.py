0bf4f6ab72d16f53618dbeec51a330508bc75d1d
Pablo Acosta-Serafini <pmasdev@gmail.com>
2019-03-05 10:39:41 -0500

    Ignore .tox, .git and other directories when looking for package name

--- functions.py
+++ functions.py
@@ -178,6 +178,10 @@ def get_pkg_name():
     stem = "pkgdata"
     start_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     for (dirpath, _, fnames) in os.walk(start_dir):
+        # Ignore .tox, .git and other directories
+        if dirpath[len(start_dir) + 1 :].startswith("."):
+            continue
+        #
         for fname in fnames:
             if os.path.splitext(os.path.basename(fname))[0] == stem:
                 return os.path.basename(dirpath)
